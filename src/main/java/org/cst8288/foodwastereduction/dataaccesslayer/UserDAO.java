9ce644de2426dc02b02ababf38a1c5dacd57e872
Ryan Xu <ryan.y.xu@hotmail.com>
2024-07-29 22:04:35 +0100

    UserDAO.java UserDAOImpl.java dispeared problem during last commit

--- src/main/java/org/cst8288/foodwastereduction/dataaccesslayer/UserDAO.java
+++ src/main/java/org/cst8288/foodwastereduction/dataaccesslayer/UserDAO.java
@@ -1,19 +1,20 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package org.cst8288.foodwastereduction.dataaccesslayer;
 
+import java.util.List;
 import org.cst8288.foodwastereduction.model.User;
 
 /**
  *
- * @author Hongxiu Guo
+ * @author ryany
  */
-public interface UserDao {
+public interface UserDAO {
 
     User getUserByEmail(String email);
-    
+    User getById(int userId);
     void addUser(User user);
     
 }
--- src/main/java/org/cst8288/foodwastereduction/dataaccesslayer/UserDAOImpl.java
+++ src/main/java/org/cst8288/foodwastereduction/dataaccesslayer/UserDAOImpl.java
@@ -10,15 +10,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.cst8288.foodwastereduction.model.User;
 import org.cst8288.foodwastereduction.constants.UserType;
+import org.cst8288.foodwastereduction.model.User;
 import org.cst8288.foodwastereduction.utility.DatetimeUtil;
 
 /**
  *
- * @author Hongxiu Guo
+ * @author ryany
  */
-public class UserDaoImpl implements UserDAO{
+public class UserDAOImpl implements UserDAO{
     
     /**
      * Gets user by user email
@@ -43,7 +43,7 @@ public class UserDaoImpl implements UserDAO{
             
             while (rs.next()) {
                 user = new User();
-                user.setUserID(rs.getInt("UserID"));
+                user.setUserId(rs.getInt("UserID"));
                 user.setName(rs.getString("Name"));
                 user.setEmail(rs.getString("Email"));
                 user.setPassword(rs.getString("PasswordHash"));
@@ -55,7 +55,7 @@ public class UserDaoImpl implements UserDAO{
             }
              
         } catch (SQLException ex) {
-            Logger.getLogger(UserDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
        return user;
     }
@@ -86,7 +86,7 @@ public class UserDaoImpl implements UserDAO{
             pstmt.executeUpdate();
             
         } catch (SQLException ex) {
-            Logger.getLogger(UserDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
@@ -107,7 +107,7 @@ public class UserDaoImpl implements UserDAO{
             
             if (rs.next()) {
                 user = new User();
-                user.setUserID(rs.getInt("UserID"));
+                user.setUserId(rs.getInt("UserID"));
                 user.setName(rs.getString("Name"));
                 user.setEmail(rs.getString("Email"));
                 user.setPassword(rs.getString("PasswordHash"));
@@ -119,17 +119,18 @@ public class UserDaoImpl implements UserDAO{
             }
              
         } catch (SQLException ex) {
-            Logger.getLogger(UserDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-            // 确保关闭资源
             try {
                 if (rs != null) rs.close();
                 if (pstmt != null) pstmt.close();
                 if (con != null) con.close();
             } catch (SQLException ex) {
-                Logger.getLogger(UserDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(UserDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         return user;
-    }    
+    }
+    
+    
 }
